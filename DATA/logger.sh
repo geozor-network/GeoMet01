2b501b9eb25bd14229b46d1422c2a46497ddbd8a
Jakub Kákona <kaklik@mlab.cz>
2013-09-09 23:07:03 +0000

    zaznam chyb zarizeni zjistenych na Islandu

--- DATA/logger.sh
+++ DATA/logger.sh
@@ -2,7 +2,7 @@
 #sudo picocom /dev/ttyUSB0 |
 while [ 1 ]; do
   if read line; then
-  echo -n "$line $(date +%Y-%m-%d-%H-%M-%S.%N) \n\r" | tee -a ./$(date +%F)_$1.log
+  echo -n "$line $(date +%Y-%m-%d-%H-%M-%S.%N) \n\r" | tee -a ./GeoMet01_$(date +%F).log
   fi
 done
 
